c4214938547e3840dedd3dd5035ff8ee4b8148c5
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-01-30 19:43:51 +0300

    Add start functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,45 @@
 package main
-import "fmt"
 
 import (
-	t "REST-API/test"
+	"context"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/pgxpool"
+	"github.com/joho/godotenv"
+	"REST-API/internal/persons/delivery"
+	"log"
+	"os"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
 func main() {
-	fmt.Println(t.Factorial(4))
-	fmt.Println(t.Factorial(95))
+	connection, state := os.LookupEnv("DATABASE_URL")
+	if !state {
+		log.Print("connection string was not found")
+		fmt.Print()
+	}
+
+	conn, err := pgxpool.Connect(context.Background(), connection)
+	if err != nil {
+		log.Fatal("database connection not established")
+	}
+
+	port, ok := os.LookupEnv("PORT")
+
+	if ok == false {
+		port = "5000"
+	}
+
+	pr := repo.NewPRepository(*conn)
+	pu := usecase.NewPUsecase(pr)
+	//pd := delivery.NewPHandler(pu)
+
+	r := mux.NewRouter()
+	r.Use(middleware.InternalServerError)
+
 }
